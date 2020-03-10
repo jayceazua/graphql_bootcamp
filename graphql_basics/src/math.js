4e1de6dcdfda8781396bc347baf19565bcbb3b78
Jayce Azua <jayce.azua@gmail.com>
2020-03-09 23:46:54 -0400

    [WIP] basic query operations and import and export lesson

--- graphql_basics/src/math.js
+++ graphql_basics/src/math.js
@@ -1,10 +1,7 @@
-const add = (x, y) => {
-  return x + y
-}
+const add = (x, y) =>  x + y
 
-const substract = (x, y) => {
-  return x - y
-}
+const substract = (x, y) => x - y
+
 
 export {
   substract,
